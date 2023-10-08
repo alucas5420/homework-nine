b5be898b292dd12024cf4c9697b262e1b9ee645e
alucas5420 <112018521+alucas5420@users.noreply.github.com>
2023-10-08 13:16:19 -0400

    Initial commit

---
+++ app/model-module.js
@@ -0,0 +1,267 @@
+var _homeContent = `<div class="site-wrapper">
+<div class="nav-holder">
+    <nav>
+        <br>
+        <br>
+        <div class="words">
+            <div class="logo">
+            Pokèmon
+        </div>
+        </div>
+    </nav>
+</div>
+<div class="content-wrapper">
+    <div class="home">
+        <div class="hero">
+            <div class="callout">
+                <h1>Pokèmon</h1>
+                <p>Welcome to the Official Pokèmon website!</p>
+                <p>Be sure to look around and discover the many different and interesting Pokèmon!</p>
+            </div>
+        </div>
+        <br>
+        <div class="promo">
+            <div class="tour-component isreal-pic background-props">
+            <div class="info-holder">
+            <h1>GAMES</h1>
+            <p>Learn more about</p>
+            <p>the Pokèmon games! </p>
+            <br>
+            <br>
+            <span>LEARN MORE</span>
+        </div>
+            </div>
+            <div class="tour-component usa-pic background-props">
+            <div class="info-holder">
+            <h1>CARDS</h1>
+            <p>Learn more about</p>
+            <p>Pokèmon cards! </p>
+            <br>
+            <br>
+            <span>LEARN MORE</span>
+        </div>
+            </div>
+            <div class="tour-component australia-pic background-props">
+            <div class="info-holder">
+            <h1>ANIME</h1>
+            <p>Learn more about</p>
+            <p>the Pokèmon anime! </p>
+            <br>
+            <br>
+            <span>LEARN MORE</span>
+        </div>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+<br>
+<footer>
+    <div class="words3">
+        Copyright 2023 Pokèmon.  All rights reserved 
+      </div>
+      
+</footer>
+</div>`;
+var _aboutContent = ` <div class="site-wrapper">
+<div class="nav-holder">
+    <nav>
+        <br>
+        <br>
+        <div class="words">
+            <div class="logo">
+            Pokèmon
+        </div>
+        </div>
+    </nav>
+</div>
+<div class="content-wrapper">
+    <div class="home">
+        <div class="hero">
+            <div class="callout">
+                <h1>Pokèmon</h1>
+                <p>Welcome to the about page!</p>
+                <p>Be sure to look around and discover the many different parts of Pokèmon history!</p>
+            </div>
+        </div>
+        <br>
+        <div class="promo">
+            <div class="tour-component kanto-pic background-props">
+            <div class="info-holder">
+            <h1>GENS 1 - 3</h1>
+            <p>Learn more about</p>
+            <p>Generations 1 - 3! </p>
+            <br>
+            <br>
+            <span>LEARN MORE</span>
+        </div>
+            </div>
+            <div class="tour-component sinnoh-pic background-props">
+            <div class="info-holder">
+            <h1>GENS 4 - 6</h1>
+            <p>Learn more about</p>
+            <p>Generations 4 - 6! </p>
+            <br>
+            <br>
+            <span>LEARN MORE</span>
+        </div>
+            </div>
+            <div class="tour-component alola-pic background-props">
+            <div class="info-holder">
+            <h1>GENS 7 - 9</h1>
+            <p>Learn more about</p>
+            <p>Generations 7 - 9! </p>
+            <br>
+            <br>
+            <span>LEARN MORE</span>
+        </div>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+<br>
+<footer>
+    <div class="words3">
+        Copyright 2023 Pokèmon.  All rights reserved 
+      </div>
+      
+</footer>
+</div>`;
+var _productsContent = `<div class="site-wrapper">
+<div class="nav-holder">
+    <nav>
+        <br>
+        <br>
+        <div class="words">
+            <div class="logo">
+            Pokèmon
+        </div>
+        </div>
+    </nav>
+</div>
+<div class="content-wrapper">
+    <div class="home">
+        <div class="hero">
+            <div class="callout">
+                <h1>Pokèmon</h1>
+                <p>Welcome to the products page!</p>
+                <p>Be sure to look around and discover the many different products that we sell!</p>
+            </div>
+        </div>
+        <br>
+        <div class="promo">
+            <div class="tour-component bdsp-pic background-props">
+            <div class="info-holder">
+            <h1>BDSP</h1>
+            <p>Learn more about</p>
+            <p>BDSP! </p>
+            <br>
+            <br>
+            <span>LEARN MORE</span>
+        </div>
+            </div>
+            <div class="tour-component legends-pic background-props">
+            <div class="info-holder">
+            <h1>Legends</h1>
+            <p>Learn more about</p>
+            <p>Legends Arceus! </p>
+            <br>
+            <br>
+            <span>LEARN MORE</span>
+        </div>
+            </div>
+            <div class="tour-component sv-pic background-props">
+            <div class="info-holder">
+            <h1>SV</h1>
+            <p>Learn more about</p>
+            <p>Scarlet and Violet! </p>
+            <br>
+            <br>
+            <span>LEARN MORE</span>
+        </div>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+<br>
+<footer>
+    <div class="words3">
+        Copyright 2023 Pokèmon.  All rights reserved 
+      </div>
+      
+</footer>
+</div>`;
+var _contactContent = `<div class="site-wrapper">
+<div class="nav-holder">
+    <nav>
+        <br>
+        <br>
+        <div class="words">
+            <div class="logo">
+            Pokèmon
+        </div>
+        </div>
+    </nav>
+</div>
+<div class="content-wrapper">
+    <div class="home">
+        <div class="hero">
+            <div class="callout">
+                <h1>Pokèmon</h1>
+                <p>Welcome to the contact page!</p>
+                <p>Be sure to look around and discover the many different ways to contact us!</p>
+            </div>
+        </div>
+        <br>
+        <div class="promo">
+            <div class="tour-component cat-pic background-props">
+            <div class="info-holder">
+            <h1>Email</h1>
+            <p>Learn more about</p>
+            <p>how to email us!</p>
+            <br>
+            <br>
+            <span>LEARN MORE</span>
+        </div>
+            </div>
+            <div class="tour-component croc-pic background-props">
+            <div class="info-holder">
+            <h1>Phone</h1>
+            <p>Learn more about</p>
+            <p>how to call us!</p>
+            <br>
+            <br>
+            <span>LEARN MORE</span>
+        </div>
+            </div>
+            <div class="tour-component duck-pic background-props">
+            <div class="info-holder">
+            <h1>Forums</h1>
+            <p>Learn more about</p>
+            <p>the Pokèmon forums! </p>
+            <br>
+            <br>
+            <span>LEARN MORE</span>
+        </div>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+<br>
+<footer>
+    <div class="words3">
+        Copyright 2023 Pokèmon.  All rights reserved 
+      </div>
+      
+</footer>
+</div>`;
+
+export function updateView(pageName){
+    console.log("Model " + pageName);
+    var pageContent = "_" + pageName;
+    $("#app").html(eval(pageContent))
+
+}
